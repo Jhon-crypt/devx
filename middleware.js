258dd68d81a3ef36a2cc6cb4637fdc4b45ab1700
jhon-crypt <johnoladele690@gmail.com>
2023-10-22 09:46:42 +0100

    Added middleware

---
+++ middleware.js
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+import { jwtVerify, createRemoteJWKSet } from "jose";
+
+const hankoApiUrl = process.env.NEXT_PUBLIC_HANKO_API_URL;
+
+export async function middleware(req) {
+    const hanko = req.cookies.get("hanko")?.value;
+
+    const JWKS = createRemoteJWKSet(
+        new URL(`${hankoApiUrl}/.well-known/jwks.json`)
+    );
+
+    try {
+        const verifiedJWT = await jwtVerify(hanko ?? "", JWKS);
+    } catch {
+        return NextResponse.redirect(new URL("/Login", req.url));
+    }
+}
+
+export const config = {
+    matcher: [
+        "/Portfolio",
+        "/Create",
+        "/editPortfolio",
+    ],
+};
