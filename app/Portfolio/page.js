eb956bcc4e215e3b8745173e95133fb9cb066c18
jhon-crypt <johnoladele690@gmail.com>
2023-10-30 16:02:44 +0100

    Updated home and about

--- app/Portfolio/page.js
+++ app/Portfolio/page.js
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 const HomeSection = dynamic(() => import('../components/section/HomeSection'), { ssr: false })
 const SidebarHeader = dynamic(() => import('../components/header/sidebarHeader'), { ssr: false })
 const StickyHeader = dynamic(() => import('../components/header/stickyHeader'), { ssr: false })
-
+import { Suspense } from "react";
 
 export default function Home() {
 
@@ -15,9 +15,15 @@ export default function Home() {
 
             <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
 
-                <StickyHeader title="Your Portfolios"/>
+                <StickyHeader title="Your Portfolios" />
 
-                <HomeSection />
+                <Suspense fallback={
+                    <div className="flex justify-center mt-20">
+                        <span className="loading loading-spinner loading-lg"></span>
+                    </div>
+                    }>
+                    <HomeSection />
+                </Suspense>
 
             </div>
 
--- app/components/section/HomeSection.js
+++ app/components/section/HomeSection.js
@@ -1,67 +1,35 @@
-'use client'
-import { useState, useEffect } from "react"
 import { AiOutlineLink, AiFillDelete } from "react-icons/ai";
 import { BiSolidEdit } from "react-icons/bi";
 import { Hanko } from "@teamhanko/hanko-elements";
 import clipboardCopy from 'clipboard-copy';
 import Link from "next/link";
 
-export default function HomeSection() {
+async function getPortfolio(id) {
+
+    const res = await fetch(`http://localhost:3000/api/fetchAllPortfolio?user_id=${id}`)
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        console.log('Failed to fetch data')
+    }
+
+    return res.json()
+
+}
+
+export default async function HomeSection() {
 
     const hankoApi = process.env.NEXT_PUBLIC_HANKO_API_URL;
     const hanko = new Hanko(hankoApi);
+    const { id } = await hanko.user.getCurrent();
 
 
-    const [portfolio, setPortfolio] = useState([])
+    const data = await getPortfolio(id)
 
-    const [loading, setLoading] = useState(false)
+    const user_portfolio = data.portfolios
 
-    const [deleteLoading, setDeleteLoading] = useState(false)
-    const [deleteStatus, setDeleteStatus] = useState(false)
-    const [deleteErrorStatus, setDeleteErrorStatus] = useState(false)
+    console.log(data)
 
-    useEffect(() => {
-
-        async function fetchPortfolio() {
-            try {
-
-                const { id } = await hanko.user.getCurrent();
-
-                setLoading(true)
-
-                let { data: portfolios, error } = await supabase
-                    .from('portfolio')
-                    .select('*')
-                    .eq("user_id", `${id}`)
-
-
-                if (portfolios) {
-
-                    if (portfolios.length === 0) {
-                        window.location.href = '/Create';
-                    } else {
-
-                        // Setting the portfolio state variable to the portfolio data
-                        setPortfolio(portfolios)
-
-                        // Setting the loading state variable to false
-                        setLoading(false)
-
-                    }
-
-                } else {
-
-                    console.log(error)
-
-                }
-            } catch (error) {
-                setLoading(false)
-            }
-        }
-
-
-        fetchPortfolio()
-    }, [])
 
     const copyToClipBoard = async (id) => {
 
@@ -71,108 +39,62 @@ export default function HomeSection() {
 
     }
 
-    const deleteSelectedPortfolio = async (id) => {
-
-        try {
-
-            setDeleteLoading(true)
-
-            const { error } = await supabase
-                .from('portfolio')
-                .delete()
-                .eq('portfolio_id', "6d45e510-1c48-401c-953d-2107d77425d5")
-
-            if (error) {
-
-                console.log(error)
-
-                setDeleteLoading(false)
-
-                setDeleteErrorStatus(true)
-
-            } else {
-
-                console.log("Deleted")
-
-                setDeleteLoading(false)
-                setDeleteStatus(true)
-
-                setPortfolio(portfolio.filter((x) => x.id != id))
-
-
-            }
-
-        } catch (error) {
-
-
-
-        }
-
-
-    }
-
 
     return (
 
         <>
-            {/*} Conditional rendering based on the value of the loading state variable{*/}
-            {loading ?
 
-                <>
-                    {/*}If loading is true, render a loading spinner{*/}
+            {/*}If loading is true, render a loading spinner
                     <div className="flex justify-center mt-20">
                         <span className="loading loading-spinner loading-lg"></span>
                     </div>
+                    {*/}
 
-                </>
-                :
-                <>
-                    {/*}If loading is false, render the portfolio items{*/}
 
-                    <div class="px-6 pt-6 2xl:container">
+            <div class="px-6 pt-6 2xl:container">
 
-                        <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
 
-                            {portfolio.map((portfolio) => (
-                                <div key={portfolio.id}>
+                    {user_portfolio.map((portfolio) => (
+                        <div key={portfolio.id}>
 
-                                    <div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
-                                        <div class="relative mx-4 mt-4 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
-                                            <div className="h-40">
+                            <div class="relative flex w-full max-w-[26rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+                                <div class="relative mx-4 mt-4 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
+                                    <div className="h-40">
 
-                                                <img
-                                                    src="/img/code2.svg"
-                                                    alt="ui/ux review check"
-                                                    class="object-cover w-full h-full"
-                                                />
+                                        <img
+                                            src="/img/code2.svg"
+                                            alt="ui/ux review check"
+                                            class="object-cover w-full h-full"
+                                        />
 
-                                            </div>
+                                    </div>
 
 
 
-                                        </div>
-                                        <div class="p-6">
-                                            <div class="mb-3 flex items-center justify-between">
-                                                <h5 class="block font-sans text-xl font-medium leading-snug tracking-normal text-blue-gray-900 antialiased">
-                                                    {portfolio.name}
-                                                </h5>
+                                </div>
+                                <div class="p-6">
+                                    <div class="mb-3 flex items-center justify-between">
+                                        <h5 class="block font-sans text-xl font-medium leading-snug tracking-normal text-blue-gray-900 antialiased">
+                                            {portfolio.name}
+                                        </h5>
 
 
-                                            </div>
+                                    </div>
 
-                                            <div class="group mt-8 inline-flex flex-wrap items-center gap-3">
+                                    <div class="group mt-8 inline-flex flex-wrap items-center gap-3">
 
-                                                <span onClick={() => copyToClipBoard(portfolio.portfolio_id)} class="cursor-pointer rounded-full border border-indigo-500/5 bg-indigo-500/5 p-3 text-indigo-500 transition-colors hover:border-indigo-500/10 hover:bg-indigo-500/10 hover:!opacity-100 group-hover:opacity-70"
-                                                >
-                                                    <AiOutlineLink class="h-5 w-5" />
-                                                </span>
-                                                <span class="cursor-pointer rounded-full border border-indigo-500/5 bg-indigo-500/5 p-3 text-indigo-500 transition-colors hover:border-indigo-500/10 hover:bg-indigo-500/10 hover:!opacity-100 group-hover:opacity-70"
-                                                >
-                                                    <Link href={`/editPortfolio/${portfolio.portfolio_id}`}>
-                                                        <BiSolidEdit class="h-5 w-5" />
-                                                    </Link>
-                                                </span>
-                                                {/*Delete button
+                                        <span onClick={() => copyToClipBoard(portfolio.portfolio_id)} class="cursor-pointer rounded-full border border-indigo-500/5 bg-indigo-500/5 p-3 text-indigo-500 transition-colors hover:border-indigo-500/10 hover:bg-indigo-500/10 hover:!opacity-100 group-hover:opacity-70"
+                                        >
+                                            <AiOutlineLink class="h-5 w-5" />
+                                        </span>
+                                        <span class="cursor-pointer rounded-full border border-indigo-500/5 bg-indigo-500/5 p-3 text-indigo-500 transition-colors hover:border-indigo-500/10 hover:bg-indigo-500/10 hover:!opacity-100 group-hover:opacity-70"
+                                        >
+                                            <Link href={`/editPortfolio/${portfolio.portfolio_id}`}>
+                                                <BiSolidEdit class="h-5 w-5" />
+                                            </Link>
+                                        </span>
+                                        {/*Delete button
                                                 <span onClick={() => deleteSelectedPortfolio(portfolio.portfolio_id)} class="cursor-pointer rounded-full border border-indigo-500/5 bg-indigo-500/5 p-3 text-indigo-500 transition-colors hover:border-indigo-500/10 hover:bg-indigo-500/10 hover:!opacity-100 group-hover:opacity-70"
                                                 >
                                                     <AiFillDelete class="h-5 w-5" />
@@ -181,33 +103,32 @@ export default function HomeSection() {
 
 
 
-                                            </div>
-                                        </div>
-                                        <div class="p-6 pt-3">
-                                            <Link href={`/dev/${portfolio.portfolio_id}`}
-                                                class="block w-full select-none rounded-lg border border-indigo-500 py-3.5 px-7 text-center font-sans text-sm font-bold uppercase text-indigo-500 hover:bg-indigo-500 hover:text-white hover:shadow-md hover:shadow-indigo-500/40 transition-all focus:opacity-85 focus:shadow-none active:opacity-85 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                                                type="button"
-                                                data-ripple-light="true"
-                                            >
-                                                View Portfolio
-                                            </Link>
-
-                                        </div>
                                     </div>
-
-
-                                    <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
+                                </div>
+                                <div class="p-6 pt-3">
+                                    <Link href={`/dev/${portfolio.portfolio_id}`}
+                                        class="block w-full select-none rounded-lg border border-indigo-500 py-3.5 px-7 text-center font-sans text-sm font-bold uppercase text-indigo-500 hover:bg-indigo-500 hover:text-white hover:shadow-md hover:shadow-indigo-500/40 transition-all focus:opacity-85 focus:shadow-none active:opacity-85 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                                        type="button"
+                                        data-ripple-light="true"
+                                    >
+                                        View Portfolio
+                                    </Link>
 
                                 </div>
-                            ))}
-                            {/*}End of col{*/}
+                            </div>
+
+
+                            <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
 
                         </div>
-                    </div>
+                    ))}
+                    {/*}End of col{*/}
 
-                </>
+                </div>
+            </div>
 
-            }
+
+
 
         </>
 
