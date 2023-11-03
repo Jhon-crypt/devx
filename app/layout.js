3aa5da320d244435fb6964bcf8a48b02efbc88f7
jhon-crypt <johnoladele690@gmail.com>
2023-11-03 01:19:14 +0100

    Added favicon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta property="og:image" content="/img/introHero2.svg" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:title" content="DevX" />
+        <meta property="og:description" content="DevX allows developers to build their personal portfolios, share their experiences, skills, projects, and other achievements in their coding journey" />
+        <meta property="og:url" content="https://devxx.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="DevX" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
